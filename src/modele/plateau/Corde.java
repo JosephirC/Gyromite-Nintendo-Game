c96a02b10e2d964d5c9f99876569b5365a438eca
p2024398 <p2024398@univ-lyon1.fr>
2022-11-30 18:02:50 +0100

    push

--- src/modele/plateau/Corde.java
+++ src/modele/plateau/Corde.java
@@ -7,10 +7,7 @@ public class Corde extends EntiteStatique {
     }
 
     public boolean peutEtreTraverse(){ return true; }
-<<<<<<< HEAD
     public boolean peutSeDeplacer() { return false; }
-=======
 
     public boolean peutPermettreDeMonterDescendre() { return true; };
->>>>>>> 0e591d9410dbeeaf4ee01d27d4be15b5719eb3d6
 }
