938e455312ea46fd7c060d519a5bb0c90129961a
p2024398 <p2024398@univ-lyon1.fr>
2022-11-30 17:54:07 +0100

    pull

--- src/modele/deplacements/ControleColonne.java
+++ src/modele/deplacements/ControleColonne.java
@@ -4,6 +4,6 @@ package modele.deplacements;
  * A la reception d'une commande, toutes les cases (EntitesDynamiques) des colonnes se déplacent dans la direction définie
  * (vérifier "collisions" avec le héros)
  */
-public class Colonne extends RealisateurDeDeplacement {
+public class ControleColonne extends RealisateurDeDeplacement {
     protected boolean realiserDeplacement() { return false; } // TODO
 }
