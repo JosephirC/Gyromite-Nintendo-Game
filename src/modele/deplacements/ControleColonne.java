937469bad1eaef402298700b3a5bdb120cbc7bbe
ekomlenovic <emilien.komlenovic@gmail.com>
2022-12-01 12:14:26 +0100

    add new class

--- src/modele/deplacements/ControleColonne.java
+++ src/modele/deplacements/ControleColonne.java
@@ -31,5 +31,62 @@ public class ControleColonne extends RealisateurDeDeplacement {
         directionCourante = _directionCourante;
     }
 
+        /* ex
+        public boolean realiserDeplacement() {
+        boolean ret = false;
+        for (EntiteDynamique e : lstEntitesDynamiques) {
+            if (e instanceof Colonne)
+                if (directionCourante != null && ((Colonne) e).get_move() != 0) {
+                    System.out.println("coucou normalement je bouge");
+                    switch (directionCourante) {
+                        case haut:
+                            Entite ehaut = e.regarderDansLaDirection(Direction.haut);
+                            if (ehaut == null) {
+                                if (e.avancerDirectionChoisie(Direction.haut))
+                                    ret = true;
+                                System.out.println("coucou je monte pas enffet lol");
+                            } else {
+                                if (ehaut.peutEtreEcrase()) {
+                                    if (ehaut instanceof EntiteVivante) {
+                                        Entite ehauthaut = ((EntiteDynamique) ehaut).regarderDansLaDirection(Direction.haut);
+                                        if (ehauthaut != null) {
+                                            ((EntiteVivante) ehaut).vivant = false;
+                                        } else {
+                                            ((EntiteVivante) ehaut).avancerDirectionChoisie(Direction.haut);
+                                            if (e.avancerDirectionChoisie(Direction.haut))
+                                                ret = true;
+                                        }
+                                    }
 
+                                }
+                            }
+                            break;
+                        case bas:
+                            Entite ebas = e.regarderDansLaDirection(Direction.bas);
+                            if (ebas == null || ebas.peutEtreEcrase()) {
+                                if (e.avancerDirectionChoisie(Direction.bas))
+                                    ret = true;
+                            } else {
+                                if (ebas.peutEtreEcrase()) {
+                                    if (ebas instanceof EntiteVivante) {
+                                        Entite ebasbas = ((EntiteDynamique) ebas).regarderDansLaDirection(Direction.bas);
+                                        if (ebasbas != null)
+                                            ((EntiteVivante) ebas).vivant = false;
+
+                                    }
+
+                                }
+                            }
+                            break;
+                    }
+                    ((Colonne) e).move();
+                } else
+                if (((Colonne) e).get_move() ==0){
+                    ((Colonne) e).init_move();
+                    this.resetDirection();
+                }
+        }
+        return ret;
+    }
+         */
 }
---
+++ src/modele/plateau/EntiteVivante.java
@@ -0,0 +1,4 @@
+package modele.plateau;
+/* pour vie ou mort des perso ptre */
+public class EntiteVivante {
+}
