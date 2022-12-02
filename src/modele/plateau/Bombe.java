20dd9ea0db6142b3ed4cd58ba417a6ccb7d2e62b
p2024398 <p2024398@univ-lyon1.fr>
2022-12-02 11:28:54 +0100

    added radis

--- src/modele/plateau/Bombe.java
+++ src/modele/plateau/Bombe.java
@@ -1,8 +1,8 @@
 package modele.plateau;
 
 public class Bombe extends EntiteRamassable{
-
     public Bombe(Jeu _jeu) {
         super(_jeu);
     }
 }
+s
---
+++ src/modele/plateau/Radis.java
@@ -0,0 +1,7 @@
+package modele.plateau;
+
+public class Radis extends EntiteRamassable{
+    public Radis(Jeu _jeu) {
+        super(_jeu);
+    }
+}
