66f9e559ac0fbc45c45fe99c840ab05f60d56360
ekomlenovic <emilien.komlenovic@gmail.com>
2022-12-06 20:50:34 +0100

    ajout de boutton non fonctionnel

--- src/modele/deplacements/ControleColonne.java
+++ src/modele/deplacements/ControleColonne.java
@@ -33,21 +33,6 @@ public class ControleColonne extends RealisateurDeDeplacement {
 
 
     public boolean realiserDeplacement() {
-
-
-        /*boolean ret = false;
-
-        for (int i = 0; i < lstEntitesDynamiques.size(); i++) {
-            EntiteDynamique e = lstEntitesDynamiques.get(i);
-            if (directionCourante != null){
-                if(e.avancerDirectionChoisie(directionCourante)) { ret = true; }
-            }
-        }
-        return ret;*/
-
-
-
-
         boolean ret = false;
         
 
@@ -118,50 +103,4 @@ public class ControleColonne extends RealisateurDeDeplacement {
     }
 
 }
-/*
-boolean ret = false;
-        for (EntiteDynamique e : lstEntitesDynamiques) {
-                if (directionCourante != null)
-                    switch (directionCourante) {
-                        case gauche:
-                            Entite eGauche = e.regarderDansLaDirection(Direction.gauche);
-                            if ((eGauche == null) || (eGauche.peutEtreTraverse())) {
-                                if (e.avancerDirectionChoisie(Direction.gauche))
-                                    ret = true;
-                            }
-                            break;
-                        case droite:
-                            Entite eDroite = e.regarderDansLaDirection(Direction.droite);
-                            if ((eDroite == null) || (eDroite.peutEtreTraverse())) {
-                                if (e.avancerDirectionChoisie(Direction.droite))
-                                    ret = true;
-                            }
-                            break;
-                        case bas:
-                            Entite eBas = e.regarderDansLaDirection(Direction.bas);
-                            if (eBas != null && (eBas.peutServirDeSupport() || eBas.peutEtreTraverse())) {
-                                if (e.avancerDirectionChoisie(Direction.bas))
-                                    ret = true;
-                            }
-                            break;
-
-                        case haut:
-                            // on ne peut pas sauter sans prendre appui
-                            // (attention, test d'appui réalisé à partir de la position courante, si la gravité à été appliquée, il ne s'agit pas de la position affichée, amélioration possible)
-                            Entite eHaut = e.regarderDansLaDirection(Direction.bas);
-                            if (eHaut != null && eHaut.peutServirDeSupport() || eHaut != null && eHaut.peutEtreTraverse()) {
-                                if (e.avancerDirectionChoisie(Direction.haut))
-                                    ret = true;
-                            }
-                            break;
-                    }
-
-        }
-
-        return ret;
-
-    }
-
- */
-
 
